dfc9dbf069098ff8a7b44db971e1856b2ef1ef85
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-10-07 03:04:59 -0300

    add none-ls plugin

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,16 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+        			null_ls.builtins.formatting.prettier.with({ filetypes = { "javascript", "typescript", "html", "css" } }),
+        			null_ls.builtins.formatting.clang_format.with({ filetypes = { "cpp", "c" } }),
+        			--null_ls.builtins.diagnostics.eslint.with({ filetypes = { "javascript", "typescript" } }),
+        			--null_ls.builtins.diagnostics.cpplint.with({ filetypes = { "cpp" } }),
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
